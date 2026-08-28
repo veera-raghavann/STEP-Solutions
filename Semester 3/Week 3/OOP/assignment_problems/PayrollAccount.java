002e6871527299e565aa78b43183ee6bc9a79af9
Veera Raghavan <veeraraghavan.manage@gmail.com>
2026-08-29 00:40:59 +0530

    Week 3: Add PayrollAccount assignment

---
+++ Semester 3/Week 3/OOP/assignment_problems/PayrollAccount.java
@@ -0,0 +1,45 @@
+package oop.assignment_problems;
+
+public class PayrollAccount {
+    private double basicSalary;
+    private double bonus;
+
+    public PayrollAccount(double openingSalary) {
+        if (openingSalary < 0) {
+            System.out.println("Warning: negative basic salary. Starting at 0.0");
+            basicSalary = 0;
+        } else {
+            basicSalary = openingSalary;
+        }
+        bonus = 0;
+    }
+
+    public void creditBonus(double amount) {
+        if (amount <= 0) {
+            System.out.println("Bonus rejected: amount must be greater than 0");
+            return;
+        }
+        bonus += amount;
+        System.out.println("Bonus credited: Rs " + amount);
+    }
+
+    public void deductTax(double percent) {
+        if (percent < 0 || percent > 100) {
+            System.out.println("Tax rejected: percent must be between 0 and 100");
+            return;
+        }
+        basicSalary -= basicSalary * percent / 100;
+        System.out.println("Tax deducted: " + percent + "%");
+    }
+
+    public double getNetSalary() {
+        return basicSalary + bonus;
+    }
+
+    public static void main(String[] args) {
+        PayrollAccount account = new PayrollAccount(50000);
+        account.creditBonus(5000);
+        account.deductTax(10);
+        System.out.println("Net salary: Rs " + account.getNetSalary());
+    }
+}
