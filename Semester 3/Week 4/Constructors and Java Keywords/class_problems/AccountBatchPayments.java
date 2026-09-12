107e88dfebe3bc590294cb23bc2246b53fca6993
Veera Raghavan <veeraraghavan.manage@gmail.com>
2026-09-12 18:41:03 +0530

    Format Week 4 Java sources

--- Semester 3/Week 4/Constructors and Java Keywords/class_problems/AccountBatchPayments.java
+++ Semester 3/Week 4/Constructors and Java Keywords/class_problems/AccountBatchPayments.java
@@ -1,1 +1,27 @@
-public class AccountBatchPayments { static class FeeAccount{double pay(double a){return a;}} static class HostelFeeAccount extends FeeAccount{double pay(double a){return a*1.02;}} public static void main(String[]a){FeeAccount[] p={new HostelFeeAccount(),new FeeAccount()};double total=0;for(FeeAccount x:p){double paid=x.pay(100);total+=paid;System.out.println("Paid: "+paid);}System.out.println("Total: "+total);} }
+public class AccountBatchPayments {
+
+    static class FeeAccount {
+        double pay(double a) {
+            return a;
+        }
+    }
+
+    static class HostelFeeAccount extends FeeAccount {
+        double pay(double a) {
+            return a * 1.02;
+        }
+    }
+
+    public static void main(String[] a) {
+        FeeAccount[] p = {new HostelFeeAccount(), new FeeAccount()};
+        double total = 0;
+
+        for (FeeAccount x : p) {
+            double paid = x.pay(100);
+            total += paid;
+            System.out.println("Paid: " + paid);
+        }
+
+        System.out.println("Total: " + total);
+    }
+}
