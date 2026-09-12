15b5e72997ad2fcbfe7f4681a72603bd84172a5a
Veera Raghavan <veeraraghavan.manage@gmail.com>
2026-09-12 18:42:39 +0530

    Format Week 5 Java sources

--- Semester 3/Week 5/Access Modifiers and Encapsulation/assignment_problems/LibraryMemberJavaBean.java
+++ Semester 3/Week 5/Access Modifiers and Encapsulation/assignment_problems/LibraryMemberJavaBean.java
@@ -1,1 +1,29 @@
-public class LibraryMemberJavaBean { private String membershipId,securityAnswerHash;public LibraryMemberJavaBean(){}public String getMembershipId(){return membershipId;}public void setMembershipId(String id){if(membershipId==null)membershipId=id;}public void setSecurityAnswer(String answer){securityAnswerHash=Integer.toHexString(answer.hashCode());}public static void main(String[]a){LibraryMemberJavaBean m=new LibraryMemberJavaBean();m.setMembershipId("LIB-1");m.setMembershipId("LIB-2");m.setSecurityAnswer("BlueMountain");System.out.println(m.getMembershipId());} }
+public class LibraryMemberJavaBean {
+    private String membershipId;
+    private String securityAnswerHash;
+
+    public LibraryMemberJavaBean() {
+    }
+
+    public String getMembershipId() {
+        return membershipId;
+    }
+
+    public void setMembershipId(String id) {
+        if (membershipId == null) {
+            membershipId = id;
+        }
+    }
+
+    public void setSecurityAnswer(String answer) {
+        securityAnswerHash = Integer.toHexString(answer.hashCode());
+    }
+
+    public static void main(String[] a) {
+        LibraryMemberJavaBean m = new LibraryMemberJavaBean();
+        m.setMembershipId("LIB-1");
+        m.setMembershipId("LIB-2");
+        m.setSecurityAnswer("BlueMountain");
+        System.out.println(m.getMembershipId());
+    }
+}
