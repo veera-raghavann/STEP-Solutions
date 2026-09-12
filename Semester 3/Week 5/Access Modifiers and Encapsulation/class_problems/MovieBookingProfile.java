fe04e5344db5fbfcdc45b89713794bc13822d67f
Veera Raghavan <veeraraghavan.manage@gmail.com>
2026-09-12 18:42:21 +0530

    Format Week 5 Java sources

--- Semester 3/Week 5/Access Modifiers and Encapsulation/class_problems/MovieBookingProfile.java
+++ Semester 3/Week 5/Access Modifiers and Encapsulation/class_problems/MovieBookingProfile.java
@@ -1,1 +1,30 @@
-public class MovieBookingProfile { private String name,otpHash;public MovieBookingProfile(){this("");}public MovieBookingProfile(String name){this.name=name;}public String getName(){return name;}public void setName(String name){this.name=name;}public void setOtp(String otp){otpHash=Integer.toHexString(otp.hashCode());}public static void main(String[]a){MovieBookingProfile p=new MovieBookingProfile("Asha");p.setOtp("1234");System.out.println(p.getName());} }
+public class MovieBookingProfile {
+    private String name;
+    private String otpHash;
+
+    public MovieBookingProfile() {
+        this("");
+    }
+
+    public MovieBookingProfile(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setOtp(String otp) {
+        otpHash = Integer.toHexString(otp.hashCode());
+    }
+
+    public static void main(String[] a) {
+        MovieBookingProfile p = new MovieBookingProfile("Asha");
+        p.setOtp("1234");
+        System.out.println(p.getName());
+    }
+}
